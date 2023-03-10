a45b1c03b53d9d06e9be63636836f24d45d8f043
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2023-03-09 20:54:05 -0500

    update-automated-save

--- day26/src/main.rs
+++ day26/src/main.rs
@@ -1,15 +1,23 @@
-fn greet<'a>(names: &mut Vec<&'a str>, target_name: &'a str, custom_message: &'a str) {
+fn greet<'a>(
+    names: &mut Vec<&'a str>,
+    target_name: &'a str,
+    custom_message: &'a str,
+    standard_message: &'a str,
+) {
     for name in names.iter_mut() {
         *name = match name {
             &mut n if n == target_name => custom_message,
-            _ => "Hello",
+            _ => standard_message,
         }
     }
 }
 
 fn main() {
-    let greeting = "well hello Ferris!";
+    let greeting = "Well, hello Ferris!";
     let mut names = vec!["Bob", "Frank", "Ferris"];
-    greet(&mut names, "Ferris", greeting);
+    greet(&mut names, "Ferris", greeting, "hi");
     println!("{:?}", names);
 }
+
+// Ref:
+// https://web.mit.edu/rust-lang_v1.25/arch/amd64_ubuntu1404/share/doc/rust/html/book/first-edition/lifetimes.html
