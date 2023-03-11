7a2dd64c650ba44e5a692e1a188a2ebf5c880cf4
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2023-03-10 19:33:35 -0500

    update-automated-save

---
+++ day27/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let names = vec!["Bob", "Frank", "Ferris"];
+
+    let r = names
+        .iter()
+        .filter_map(|name| match name.starts_with("F") {
+            true => Some(name),
+            false => None,
+        })
+        .map(|name| name.to_uppercase())
+        .collect::<Vec<String>>();
+
+    println!("{:?}", r);
+}
