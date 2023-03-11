e82abf23405ad3423ff0f2bfcc1b0f7e36bebddd
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2023-03-11 18:00:21 -0500

    update-automated-save

--- each_day/day28/src/main.rs
+++ each_day/day28/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn read_file(path: &str) -> String {
+    let mut file = match File::open(path) {
+        Ok(file) => file,
+        Err(e) => panic!("Error opening file: {:?}", e),
+    };
+
+    let mut contents = String::new();
+    match file.read_to_string(&mut contents) {
+        Ok(_) => println!("{}", contents),
+        Err(e) => panic!("Error reading file: {:?}", e),
+    }
+    contents
+}
+
 fn modify_names(names: &mut Vec<&str>, custom_message: &str) {
     names.iter_mut().for_each(|i| {
         let new_name = match *i {
@@ -9,9 +26,11 @@ fn modify_names(names: &mut Vec<&str>, custom_message: &str) {
 }
 
 fn main() {
-    let mut names = vec!["Bob", "Frank", "Ferris"];
+    let input_string = read_file("./data/file.txt");
 
-    modify_names(&mut names, ", custom message!");
+    let mut lines: Vec<&str> = input_string.lines().collect();
 
-    println!("{:?}", names);
+    modify_names(&mut lines, ", custom message!");
+
+    println!("{:?}", lines);
 }
