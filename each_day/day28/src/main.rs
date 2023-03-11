a77a9b7f433322355fd38b872be8977b5d1c6fab
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2023-03-11 11:25:08 -0500

    update-automated-save

--- each_day/day28/src/main.rs
+++ each_day/day28/src/main.rs
@@ -1,8 +1,8 @@
-fn modify_names(names: &mut Vec<&str>) {
+fn modify_names(names: &mut Vec<&str>, custom_message: &str) {
     names.iter_mut().for_each(|i| {
         let new_name = match *i {
             "Bob" => format!("{}!, What's happening?", *i),
-            _ => format!("Hi {}", *i),
+            _ => format!("Hi {} {}", *i, custom_message),
         };
         *i = Box::leak(new_name.into_boxed_str());
     });
@@ -11,7 +11,7 @@ fn modify_names(names: &mut Vec<&str>) {
 fn main() {
     let mut names = vec!["Bob", "Frank", "Ferris"];
 
-    modify_names(&mut names);
+    modify_names(&mut names, ", custom message!");
 
     println!("{:?}", names);
 }
