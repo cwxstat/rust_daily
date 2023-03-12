69ef1e9874204d8875d7ec83f57da865a2b8da52
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2023-03-12 10:48:26 -0400

    update-automated-save

--- each_day/day28/src/main.rs
+++ each_day/day28/src/main.rs
@@ -1,18 +1,11 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn read_file(path: &str) -> String {
-    let mut file = match File::open(path) {
-        Ok(file) => file,
-        Err(e) => panic!("Error opening file: {:?}", e),
-    };
-
+fn read_file(path: &str) -> Result<String, std::io::Error> {
+    let mut file = File::open(path)?;
     let mut contents = String::new();
-    match file.read_to_string(&mut contents) {
-        Ok(_) => {}
-        Err(e) => panic!("Error reading file: {:?}", e),
-    }
-    contents
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
 }
 
 fn modify_names(names: &mut Vec<&str>, custom_message: &str) {
@@ -26,11 +19,12 @@ fn modify_names(names: &mut Vec<&str>, custom_message: &str) {
 }
 
 fn main() {
-    let input_string = read_file("./data/file.txt");
-
-    let mut lines: Vec<&str> = input_string.lines().collect();
-
-    modify_names(&mut lines, ", custom message!");
-
-    println!("{:?}", lines);
+    match read_file("./data/file.txt") {
+        Ok(contents) => {
+            let mut lines = contents.lines().collect();
+            modify_names(&mut lines, ", custom message!");
+            println!("{:?}", lines);
+        }
+        Err(e) => panic!("Error reading file: {:?}", e),
+    }
 }
