936f918ad8e9f43b5eff9d2c3a847a25e3ce5d42
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2023-03-16 09:06:40 -0400

    update-automated-save

--- each_day/day32c/src/main.rs
+++ each_day/day32c/src/main.rs
@@ -1,54 +1,73 @@
-use std::process::Command;
 use std::env;
-use std::str;
 use std::fs;
+use std::path::PathBuf;
 
 fn main() {
     let home_dir = env::var("HOME").expect("Failed to get user's home directory");
     let downloads_dir = format!("{}/Downloads", home_dir);
     let pdf_dir = format!("{}/pdf", downloads_dir);
 
-    // Create the spork directory if it doesn't exist
+    // Create the directory if it doesn't exist
     if let Err(_) = fs::create_dir_all(&pdf_dir) {
         eprintln!("Failed to create directory: {}", pdf_dir);
         return;
     }
 
-    let output = Command::new("ls")
-        .arg("-l")
-        .arg(&downloads_dir)
-        .output()
-        .expect("Failed to execute command");
+    let entries = match fs::read_dir(&downloads_dir) {
+        Ok(entries) => entries,
+        Err(e) => {
+            eprintln!("Failed to read directory: {}\nError: {}", downloads_dir, e);
+            return;
+        }
+    };
 
-    if output.status.success() {
-        let result = str::from_utf8(&output.stdout).unwrap();
-        let pdf_files = result
-            .lines()
-            .filter(|line| line.contains("pdf"))
-            .collect::<Vec<&str>>();
+    let mut file_count = 0;
+    let mut total_bytes_moved = 0;
 
-        let mut file_count = 0;
-        let mut total_bytes_moved = 0;
+    for entry in entries {
+        if let Ok(entry) = entry {
+            let metadata = match entry.metadata() {
+                Ok(metadata) => metadata,
+                Err(e) => {
+                    eprintln!("Failed to get metadata: {}", e);
+                    continue;
+                }
+            };
 
-        for entry in pdf_files {
-            let fields: Vec<&str> = entry.split_whitespace().collect();
-            let file_size: u64 = fields[4].parse().unwrap();
-            let file_name = fields.last().unwrap();
+            if metadata.is_file() {
+                let file_name = entry.file_name();
+                let file_name_str = file_name.to_string_lossy();
 
-            let source_path = format!("{}/{}", downloads_dir, file_name);
-            let destination_path = format!("{}/{}", pdf_dir, file_name);
+                if file_name_str.contains("pdf") {
+                    let source_path = entry.path();
+                    let destination_path = PathBuf::from(&pdf_dir).join(file_name.clone());
 
-            if let Err(e) = fs::rename(&source_path, &destination_path) {
-                eprintln!("Failed to move file: {}\nError: {}", file_name, e);
-            } else {
-                println!("Moved file: {}", file_name);
-                file_count += 1;
-                total_bytes_moved += file_size;
+                    match entry.metadata() {
+                        Ok(metadata) => {
+                            let file_size = metadata.len();
+
+                            if let Err(e) = fs::rename(&source_path, &destination_path) {
+                                eprintln!("Failed to move file: {}\nError: {}", file_name_str, e);
+                            } else {
+                                println!("Moved file: {}", file_name_str);
+                                file_count += 1;
+                                total_bytes_moved += file_size;
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!(
+                                "Failed to get metadata for file: {}\nError: {}",
+                                file_name_str, e
+                            );
+                        }
+                    }
+                }
             }
         }
-
-        println!("Moved {} file(s), total bytes: {}", file_count, total_bytes_moved);
-    } else {
-        eprintln!("Error: {:?}", output.stderr);
     }
+
+    println!(
+        "Moved {} file(s), total bytes: {}",
+        file_count, total_bytes_moved
+    );
 }
